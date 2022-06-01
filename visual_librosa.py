23f8ce3679268ed2a38e07b4504e888764c62973
WIN-S9C0KGGALPP\lega <legavaz@gmail.com>
2022-06-01 17:23:51 +0300

    +

--- visual_librosa.py
+++ visual_librosa.py
@@ -4,7 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import glob
 
-import librosa as lr
+# https://librosa.org/doc/0.7.2/tutorial.html
+import librosa as lr 
 
 # Variable
 file_name = r"tmp\20210227_135213_test.m4a"
